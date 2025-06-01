f0cbc09654b15a8b24d05270b75f50a4e9504502
alejandrojmnzz <alejandro12850mj@gmail.com>
2025-06-01 22:41:05 +0000

    env variables

--- user-registration-frontend/src/services/user.js
+++ user-registration-frontend/src/services/user.js
@@ -1,13 +1,14 @@
 import axios from "axios"
+
 //En este archivo se utiliza axios para hacer las peticiones necesarias al servidor backend de Express
-const url = "https://shiny-spoon-q79r9q767qg4f6xg-3001.app.github.dev"
+const url = import.meta.env.VITE_BACKEND_URL
 
 function postUser(user) {
-    return axios.post(url + "/api/users", user)
+    return axios.post(url + "api/users", user)
 }
 
 function getUsers() {
-    return axios.get(url + "/api/users")
+    return axios.get(url + "api/users")
 }
 
 export default {postUser, getUsers}
